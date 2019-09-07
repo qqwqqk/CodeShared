e074dbbb1c5fb0d0ab15f254941bc6366637f463
qqwqqk <qqwqqk@shu.edu.cn>
2019-09-07 18:04:38 +0800

    11

--- qqwqqk/main.cpp
+++ qqwqqk/main.cpp
@@ -1,15 +1,46 @@
 #include<stdio.h>
 #include<iostream>
 #include<vector>
+#include<queue>
+#include<algorithm>
 using namespace std;
+
+bool sortFun(const int &p1, const int &p2){
+	return p1 > p2;
+}
  
 int main()
 {
-  vector<int> num;
-  int n,m;
-  cin >> n >> m;
+  vector<int> prices = {4,2};
+  int budget = 7;
 
-  int result = n+m;
+  int result = -1;
+  queue<int> lists;
+
+  sort(prices.begin(), prices.end(), sortFun);
+
+  int i=0;
+  int j=0;
+
+  while(1){
+    if(budget >= prices[i]){
+      lists.push(prices[i]);
+      budget = budget - prices[i];
+      if(budget == 0){ result=lists.size(); break; }
+    } else {
+      i++;
+      if(i < prices.size()){ continue; }
+      else {
+        if(lists.front() > prices[j]){
+          budget += lists.front();
+          lists.pop();
+          j++;
+          i = j;
+        } else { break; }
+      }
+    }
+  }
+
   cout << result <<endl;
  
 	return 0;
