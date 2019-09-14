409092995b52108a550607af97462f686d9c2d49
qqwqqk <qqwqqk@shu.edu.cn>
2019-09-14 16:13:21 +0800

    add red 3

--- qqwqqk/main.cpp
+++ qqwqqk/main.cpp
@@ -6,75 +6,45 @@
 #include<algorithm>
 using namespace std;
 
-vector<int> analyzeLine(string str){
-  int strSub = 0, strPos = 0;
-  string cache = "";
-  char split = ' ';
-  vector<int> result;
-
-  for(int i = 0 ; i < str.size(); i++){
-    if(str[i] == split){
-      strSub = i - strPos;
-      cache = str.substr(strPos, strSub);
-      result.push_back(stoi(cache));
-      strPos = i+1;
-    }
-  }
-  cache = str.substr(strPos, str.size() - strPos);
-  if(cache.size()>0){ result.push_back(stoi(cache)); }
-
-  return result;
-}
-
-class people{
+class treasure{
 public:
-  int id;
-  bool tag;
-  vector<int> friends;
-  people(int i = -1){ id = i; tag = false; }
+  int x,h;
+  int rank;
+  treasure(int i=-1,int j=-1){
+    x = i, h = j;
+    rank = x>h?x:h;
+  }
 };
 
-int getPeople(vector<people> list){
-  int res = -1;
-  for(int i=0; i<list.size(); i++){
-    if(!list[i].tag){res = i; break;}
+bool sortFun(treasure &t1, treasure &t2){
+  if(t1.rank == t2.rank){
+    if(t1.x == t2.x){
+      return t1.h < t2.h;
+    }
+    return t1.x < t2.x;
   }
-  return res;
+  return t1.rank < t2.rank;
 }
 
 int main () {
   int n;
-  string str;
+  int x,h;
+  int result=0;
+  vector<treasure> lists;
 
   cin >> n;
-  getchar();
-  vector<people> lists;
-
   for(int i=0; i<n; i++){
-    getline(cin, str,'\n');
-    vector<int> cache = analyzeLine(str);
-    people item(i);
-    for(int j=0; j<n; j++){ if(cache[j] == 1){ item.friends.push_back(j);} }
+    cin>>x>>h;
+    treasure item(x,h);
     lists.push_back(item);
   }
+  sort(lists.begin(), lists.end(), sortFun);
 
-  int result = 0;
-  while(1){
-    int userid = getPeople(lists);
-    if(userid == -1){break;}
-    lists[userid].tag = true;
-    result++ ;
-    vector<int> cache = lists[userid].friends;
-    queue<int> temp;
-    for(int i=0; i<cache.size(); i++){ temp.push(cache[i]); }
-    while(!temp.empty()){
-      int id = temp.front();
-      lists[id].tag = true;
-      temp.pop();
-      vector<int> items = lists[id].friends;
-      for(int i=0; i<items.size(); i++){ 
-        if(!lists[items[i]].tag){temp.push(items[i]);}
-      }
+  int rank = -1;
+  for(int i=0; i<n; i++){
+    if(lists[i].x >= rank && lists[i].h >= rank){
+      result++;
+      rank = lists[i].rank;
     }
   }
 
