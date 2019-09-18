ce0d428837481f029d4f91810dbac5dec936beb2
qqwqqk <qqwqqk@shu.edu.cn>
2019-09-18 16:30:37 +0800

    add meituan

--- qqwqqk/jest.js
+++ qqwqqk/jest.js
@@ -76,3 +76,20 @@ if(age >12){
 }
 
 console.log("${years} years");
+
+var name = "global";
+var obj = {
+  name:"local",
+  foo: function(){
+    this.name = "foo";
+  }.bind(window)
+};
+var bar = new obj.foo();
+
+setTimeout(function(){
+  console.log(window.name);
+},0);
+console.log(bar.name);
+var bar3 = bar2 = bar;
+bar2.name = "foo2";
+console.log(bar3.name);
