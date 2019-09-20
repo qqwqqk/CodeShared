27cc6466e4fe0c33543ec4e11f5c4ca512e6098a
qqwqqk <qqwqqk@shu.edu.cn>
2019-09-20 21:03:18 +0800

    1

--- zjh/tengxun/2.cpp
+++ zjh/tengxun/2.cpp
@@ -22,26 +22,39 @@ int main(){
 
     cout<<y<<"-";
     if(m==1||m==3||m==5||m==7||m==8||m==12){//31
+        if(m<=9)
+          cout<<"0";
         cout<<m<<"-"<<d<<endl;cout<<"1"<<endl;
     }else if(m==4||m==6||m==9||m==10||m==11){
+        if(m<=9)
+          cout<<"0";
         if(d==31){
             d=1;
-            cout<<m+1<<"-"<<d<<endl;
+            cout<<m+1<<"-"<<"0"<<d<<endl;
         }else{
-            cout<<m<<"-"<<d<<endl;
+            if(d<=9)
+              cout<<m<<"-0"<<d<<endl;
+            else
+              cout<<m<<"-"<<d<<endl;
         }
     }else{
         if(isRunY==true){
             if(d>29){
-                cout<<m+1<<"-"<<d-29<<endl;
+                cout<<"0"<<m+1<<"-0"<<d-29<<endl;
             }else{
-                cout<<m<<"-"<<d<<endl;
+                if(d<=9)
+                  cout<<"0"<<m<<"-0"<<d<<endl;
+                else
+                  cout<<"0"<<m<<"-"<<d<<endl;
             }
         }else{
             if(d>28){
-                cout<<m+1<<"-"<<28-d<<endl;
+                cout<<"0"<<m+1<<"-0"<<28-d<<endl;
             }else{
-                cout<<m<<"-"<<d<<endl;
+                if(d<=9)
+                  cout<<"0"<<m<<"-0"<<d<<endl;
+                else
+                  cout<<"0"<<m<<"-"<<d<<endl;
             }
         }
     }
