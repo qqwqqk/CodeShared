8de0d5581c569cf9b6e523234540c6468cdca855
qqwqqk <qqwqqk@shu.edu.cn>
2019-09-20 21:21:55 +0800

    add 1

--- qqwqqk/other/tx1.cpp
+++ qqwqqk/other/tx1.cpp
@@ -12,9 +12,24 @@ int sub(vector<int> &list, int x, int y){
     if(list[i] == x){x_index = i; if(y_index > -1){ break; }}
     if(list[i] == y){y_index = i; if(x_index > -1){ break; }}
   }
-  int sub_1 = x_index < y_index ? x_index - 0 : y_index - 0;
-  int sub_2 = x_index < y_index ? y_index - x_index -1 : x_index - y_index - 1;
-  int sub_3 = x_index < y_index ? n - y_index - 1: n - x_index - 1;
+
+  int sub_1=0;
+  int sub_2=0;
+  int sub_3=0;
+
+  if(x_index == -1 && y_index == -1){
+    return sub_1 = n;
+  } else if(x_index == -1){
+    sub_1 = y_index - 0;
+    sub_2 = n - 1 - y_index;
+  } else if(y_index == -1){
+    sub_1 = x_index - 0;
+    sub_2 = n - 1 - x_index;
+  } else {
+    sub_1 = x_index < y_index ? x_index - 0 : y_index - 0;
+    sub_2 = x_index < y_index ? y_index - x_index -1 : x_index - y_index - 1;
+    sub_3 = x_index < y_index ? n - y_index - 1: n - x_index - 1;
+  }
 
   int res = 0;
   res += sub_1*(sub_1+1)/2;
