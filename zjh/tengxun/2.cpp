95bcc4606c4f6d166d0c65c566837292f203b7eb
qqwqqk <qqwqqk@shu.edu.cn>
2019-09-20 21:08:34 +0800

    1

--- zjh/tengxun/2.cpp
+++ zjh/tengxun/2.cpp
@@ -24,7 +24,7 @@ int main(){
     if(m==1||m==3||m==5||m==7||m==8||m==12){//31
         if(m<=9)
           cout<<"0";
-        cout<<m<<"-"<<d<<endl;cout<<"1"<<endl;
+        cout<<m<<"-"<<d<<endl;
     }else if(m==4||m==6||m==9||m==10||m==11){
         if(m<=9)
           cout<<"0";
