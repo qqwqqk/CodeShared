6a7290c786066cff0ce2ba9676d4eaa744e80b60
qqwqqk <qqwqqk@shu.edu.cn>
2019-10-17 19:49:39 +0800

    add i

--- qqwqqk/main.cpp
+++ qqwqqk/main.cpp
@@ -1,46 +1,29 @@
-#include <iostream>
-#include <vector>
-#include <set>
-#include <iomanip>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 using namespace std;
 
-int main() {
-  int N;
-  int n;
-  int num;
-  int count;
-  bool result = true;
-  set<int> list;
+int main(void){
+  int n,min;
+  vector<int> list;
+  cin>>n>>min;
 
-  cin >> N;
+  int temp;
+  for(int i=0; i<n;i++){
+    cin>>temp;
+    list.push_back(temp);
+  }
+  sort(list.begin(), list.end());
 
-  for(int i=0; i<N; i++){
-    cin >> num;
-    if(num != 0){
-      if(list.count(num) > 0){ result = false; } 
-      else { list.insert(num); }
-    } else {
-      n++;
-    }
+  int res = 0;
+  int i=0, j=list.size();
+
+  while(i<j){
+    if(i+j<=min){i++;j--;}else{j--;}
+    res++;
   }
 
-  set<int>::iterator iter = list.begin();
-  int font, next;
-  while(1){
-    font = *iter;
-    iter++;
-    if(iter == list.end()){ break; }
-    next = *iter;
-    if(next - font > 1){
-      count += next - font - 1;
-    }
-  }
-
-  if(count <= n && result){
-    cout << "YES+"<<n;
-  } else {
-    cout << "NO+"<<n;
-  }
+  cout<<res;
 
   return 0;
 }
